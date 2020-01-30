60cb7301fadf0d2443bfb0c1319a20946389527d
Camille <zaugc@seattleu.edu>
2020-01-30 10:50:44 -0800

    Create sort.py
    Sad

---
+++ sort.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+#vector = np.random.randint(10,size=10)
+#vector = [6, 9, 3, 0, 4, 6, 3, 2, 1, 4]
+#vector = [2,1,4]
+vector = [0,1,2,3,4,5,6,7,8,9]
+
+print(vector)
+
+def f(vect):
+
+    if len(vect)>1:
+        a=vect[-1]
+        vectnew = vect[:-1]
+        b = vectnew[-1]
+        print(vectnew)
+        if a>=b:
+            print(a,b)
+            f(vectnew)
+        else:
+            return False
+    else:
+        return True
+
+var = f(vector)
+
+if var is True:
+    print('yes')
+else:
+    print('no')
